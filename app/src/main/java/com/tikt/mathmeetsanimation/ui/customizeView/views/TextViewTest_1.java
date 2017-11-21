56feb532b7c11290ed4228611246bc44f7055293
tikt <tikt@debian.localhost>
2017-11-21 11:59:55 +0800

    给textview绘制背景，利用Matrix添加动态闪动效果

--- app/src/main/java/com/tikt/mathmeetsanimation/ui/customizeView/views/TextViewTest_1.java
+++ app/src/main/java/com/tikt/mathmeetsanimation/ui/customizeView/views/TextViewTest_1.java
@@ -3,7 +3,10 @@ package com.tikt.mathmeetsanimation.ui.customizeView.views;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
@@ -15,9 +18,19 @@ import android.util.AttributeSet;
 
 public class TextViewTest_1 extends AppCompatTextView {
 
+    /*
+    给TextView绘制背景
+     */
     Paint mPaint1;
     Paint mPaint2;
-
+    private int mViewWidth;
+    private Paint mPaint;
+    /*
+    使用LinearGradient Shader和Matrix实现一个动态的文字闪动效果
+     */
+    private Matrix mGradientMatrix;
+    private LinearGradient mLinearGradient;
+    private int mTranslate;
 
 
     public TextViewTest_1(Context context) {
@@ -52,5 +65,56 @@ public class TextViewTest_1 extends AppCompatTextView {
         //父类完成的方法，即绘制文本
         super.onDraw(canvas);
         canvas.restore();
+
+        /**
+         * 最关键的就是使用getPaint()方法获取当前绘制TextView的Paint对象，
+         * 并给Paint对象设置原生TextView没有的LinearGradient属性。
+         * 之后通过矩阵的方式来不断平移渐变效果，从而在绘制文字时产生动态的闪动效果。
+         */
+
+        if(mGradientMatrix != null){
+            mTranslate += mViewWidth/5;
+            if(mTranslate >2*mViewWidth){
+                mTranslate =- mViewWidth;
+            }
+            mGradientMatrix.setTranslate(mTranslate,0);
+            mLinearGradient.setLocalMatrix(mGradientMatrix);
+            postInvalidateDelayed(50);
+
+
+        }
+
+
+
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        if(mViewWidth==0){
+            mViewWidth = getMeasuredWidth();
+            if(mViewWidth>0){
+                mPaint = getPaint();
+                mLinearGradient = new LinearGradient(0,0,mViewWidth,0,
+                        new int[]{Color.BLUE,0xffffffff,Color.BLUE},null, Shader.TileMode.CLAMP);
+                mPaint.setShader(mLinearGradient);
+                mGradientMatrix = new Matrix();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
